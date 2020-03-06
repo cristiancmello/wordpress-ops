e6277d8f2fd600ad23dbfaae727ddd6107c72bea
Cristian Mello <cristianc.mello@gmail.com>
2020-03-06 11:35:14 -0300

    Add wordpress ops vm specs

---
+++ setup-docker-swarm-portainer.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Initialize Docker Swarm Master Node
+docker swarm init
+
+# Install Portainer
+docker volume create portainer_data
+docker run -d -p 9000:9000 -p 8000:8000 --name portainer --restart always \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v portainer_data:/data portainer/portainer
