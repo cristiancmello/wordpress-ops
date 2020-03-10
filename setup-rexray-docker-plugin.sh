3b7afce8d3a327b711eb570bc08d2e9c9e4db945
Cristian Mello <cristianc.mello@gmail.com>
2020-03-10 20:37:12 -0300

    Add s3fs docker plugin installation

--- setup-rexray-docker-plugin.sh
+++ setup-rexray-docker-plugin.sh
@@ -3,11 +3,14 @@
 sudo systemctl restart rexray
 sleep 3
 
-sudo systemctl status rexray
-
 docker plugin install rexray/ebs \
     REXRAY_PREEMPT=true \
     EBS_REGION=$EBS_REGION \
     EBS_ACCESSKEY=$EBS_ACCESSKEY \
     EBS_SECRETKEY=$EBS_SECRETKEY \
+    --grant-all-permissions
+
+docker plugin install rexray/s3fs \
+    S3FS_ACCESSKEY=$EBS_ACCESSKEY \
+    S3FS_SECRETKEY=$EBS_SECRETKEY \
     --grant-all-permissions
