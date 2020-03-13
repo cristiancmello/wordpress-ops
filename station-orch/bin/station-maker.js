94a4eb9c25783d64ae9861f95a568a539b6cb35f
Cristian Mello <cristianc.mello@gmail.com>
2020-03-13 14:12:54 -0300

    Add outputdir

--- station-orch/bin/station-maker.js
+++ station-orch/bin/station-maker.js
@@ -3,6 +3,8 @@
 const cdk = require("@aws-cdk/core");
 const { StationMakerStack } = require("../lib/station-maker-stack");
 
+console.log(process.env.CDK_DEFAULT_ACCOUNT);
+
 const app = new cdk.App();
 new StationMakerStack(app, "StationMakerStack", {
   env: {
