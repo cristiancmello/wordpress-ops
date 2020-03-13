00f582bb606775dbc5437a90d949d234afaf91c5
Cristian Mello <cristianc.mello@gmail.com>
2020-03-13 09:59:39 -0300

    Add experimental serverless rest api

---
+++ station-orch/handler.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const cdk = require("@aws-cdk/core");
+
+module.exports.deploy = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v1.0! Your function executed successfully!",
+        input: event
+      },
+      null,
+      2
+    )
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
