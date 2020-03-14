abd047ac06082f0a8701b102d7109c667394d49d
Cristian Mello <cristianc.mello@gmail.com>
2020-03-14 18:11:28 -0300

    Add station input test

--- station-orch/functions/cdkDeploy.js
+++ station-orch/functions/cdkDeploy.js
@@ -2,6 +2,7 @@
 
 const AWS = require("aws-sdk");
 const { exec, pwd, env } = require("shelljs");
+const fs = require("fs");
 
 module.exports.handler = async event => {
   const requestId = event.requestId;
@@ -19,6 +20,14 @@ region=${credentials.aws_region}
 output=json
 EOF"
   `);
+
+  // Generate station.input.json
+  fs.writeFileSync(
+    "/tmp/cdk.out/station.input.json",
+    JSON.stringify({
+      profileName
+    })
+  );
 
   const cdkExec = exec(
     `./node_modules/cdk/bin/cdk deploy -o /tmp/cdk.out --plugin cdk-profile-plugin --require-approval never`,
