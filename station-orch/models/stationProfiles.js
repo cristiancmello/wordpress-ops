501d4933fd1fae01ecd03393a5aebbc8205a47c6
Cristian Mello <cristianc.mello@gmail.com>
2020-03-20 14:15:18 -0300

    Add stationProfiles model

---
+++ station-orch/models/stationProfiles.js
@@ -0,0 +1,43 @@
+const {
+  DynamoDbSchema,
+  DynamoDbTable,
+  embed
+} = require("@aws/dynamodb-data-mapper");
+
+const uuid = require("uuid");
+
+class StationProfile {
+  constructor() {}
+}
+
+Object.defineProperties(StationProfile.prototype, {
+  [DynamoDbTable]: {
+    value: "stationProfiles"
+  },
+  [DynamoDbSchema]: {
+    value: {
+      id: {
+        type: "String",
+        keyType: "HASH",
+        defaultProvider: uuid.v4
+      },
+      version: {
+        type: "String"
+      },
+      publicName: {
+        type: "String"
+      },
+      privateName: {
+        type: "String"
+      },
+      privateDescription: {
+        type: "String"
+      },
+      properties: {
+        type: "String"
+      }
+    }
+  }
+});
+
+module.exports = StationProfile;
