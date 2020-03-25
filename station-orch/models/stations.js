b1b3a1cf752c1cc1ae781414ee59ae475f9f2430
Cristian Mello <cristianc.mello@gmail.com>
2020-03-25 12:54:41 -0300

    Add Station findFirstById method

--- station-orch/models/stations.js
+++ station-orch/models/stations.js
@@ -8,8 +8,37 @@ const randomstring = require("randomstring");
 
 const uuid = require("uuid");
 
+const { DataMapper } = require("@aws/dynamodb-data-mapper");
+const DynamoDB = require("aws-sdk/clients/dynamodb");
+const { equals } = require("@aws/dynamodb-expressions");
+
+const client = new DynamoDB({ region: "us-east-1" });
+const mapper = new DataMapper({ client });
+
 class Station {
   constructor() {}
+
+  static findFirstById = async id => {
+    const station = new this();
+
+    station.id = id;
+
+    const stations = mapper.scan(this, {
+      limit: 1,
+      filter: {
+        ...equals(station.id),
+        subject: "id"
+      }
+    });
+
+    const firstStation = async stations => {
+      for await (const record of stations) {
+        return record;
+      }
+    };
+
+    return firstStation(stations);
+  };
 }
 
 const randomString = () => {
